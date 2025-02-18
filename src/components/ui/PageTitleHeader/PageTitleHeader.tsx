75dee80840f51414c72e3558abf57ededa1b555b
Ife Onuorah <ifeonuorah@gmail.com>
2025-02-18 12:04:51 -0400

    Change width of max content to 1440px, add mixins for tablet & desktop screen sizes, make Page title header responsive and update Fitness waiver to be responsive

--- src/components/ui/PageTitleHeader/PageTitleHeader.tsx
+++ src/components/ui/PageTitleHeader/PageTitleHeader.tsx
@@ -7,8 +7,10 @@ type pageHeader = {
 export default function PageTitleHeader({ title, subHeading }: pageHeader) {
 	return (
 		<div className={styles.header}>
-			<h1 className={styles.header__title}>{title}</h1>
-			<h2 className={styles.header__subHeading}>{subHeading}</h2>
+			<div className={styles.wrapper}>
+				<h1 className={styles.header__title}>{title}</h1>
+				<h2 className={styles.header__subHeading}>{subHeading}</h2>
+			</div>
 		</div>
 	);
 }
