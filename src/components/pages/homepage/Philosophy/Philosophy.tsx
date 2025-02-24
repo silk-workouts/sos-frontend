8cc20e7bc36659399d67f08d80056462368649a6
betelhemalemayehu <Betelhem.L.A@gmail.com>
2025-02-24 17:14:47 -0500

    homepage: mobile design for philosphy section set to column reverse

--- src/components/pages/homepage/Philosophy/Philosophy.tsx
+++ src/components/pages/homepage/Philosophy/Philosophy.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import jumpRopeImage from '/public/assets/images/jumpRope.jpg';
-import styles from './Philosophy.module.scss';
+import Image from "next/image";
+import jumpRopeImage from "/public/assets/images/jumpRope.png";
+import styles from "./Philosophy.module.scss";
 
 export default function Philosophy() {
   return (
@@ -10,10 +10,14 @@ export default function Philosophy() {
       <div className={styles.founder}>
         <Image src={jumpRopeImage} alt="A picture of Michael jump roping" />
         <div className={styles.founder__name}>
-          <span className="text-small">michael olajide, founder & trainer</span>
+          <span className="text-small">
+            michael olajide,
+            <br className={styles.lineBreak} />
+            founder & trainer
+          </span>
         </div>
       </div>
-      <div className={styles['philosophy-container']}>
+      <div className={styles["philosophy-container"]}>
         <h3 className="h3_title h3_title--accent">our philosophy</h3>
         <div className={styles.philosophy__info}>
           <h4 className="h2-title">Do It Different</h4>
