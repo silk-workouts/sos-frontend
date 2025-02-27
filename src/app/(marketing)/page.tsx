08a1d57873792630c57d39774a27c8b594e2e96f
betelhemalemayehu <Betelhem.L.A@gmail.com>
2025-02-27 00:11:42 -0500

    homepage: formulate your workout section created, along with updates to element/about/philosophy/whatsSOS to match mockup for mobile

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,26 +4,17 @@ import HomeHero from "@/components/pages/homepage/HomeHero/HomeHero";
 import Philosophy from "@/components/pages/homepage/Philosophy/Philosophy";
 import About from "@/components/pages/homepage/About/About";
 import GetStarted from "@/components/pages/homepage/GetStarted/GetStarted";
+import WhatsSOS from "@/components/pages/homepage/WhatsSOS/WhatsSOS";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
       <Philosophy />
-      {/* <HomeElements /> */}
-      {/* {/* <div className={styles.slogan}> */}
-      {/* <span className={styles.slogan__message}>
-        100% Silk fitness is key. 100% Silk fitness is key. 100% Silk fitness is
-        key. 100% Silk fitness is key. 100% Silk fitness is key.
-      </span>
-      <span
-        className={`${styles.slogan__message} ${styles["slogan__message--bottom"]}`}
-      >
-        100% Silk fitness is key. 100% Silk fitness is key. 100% Silk fitness is
-        key. 100% Silk fitness is key. 100% Silk fitness is key.
-      </span> */}
 
       <About />
+      <WhatsSOS />
+      <HomeElements />
       <GetStarted />
     </>
   );
