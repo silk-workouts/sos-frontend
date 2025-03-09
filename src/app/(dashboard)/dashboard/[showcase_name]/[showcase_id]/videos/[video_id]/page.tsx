87889691d1e373008b953c73a0710d52a360d7fa
Matt Meyer <mattjacobnyc@gmail.com>
2025-03-09 15:08:06 -0400

    modify API urls used with axios to make agnostic (best practice for Next.js app serving API data

--- src/app/(dashboard)/dashboard/[showcase_name]/[showcase_id]/videos/[video_id]/page.tsx
+++ src/app/(dashboard)/dashboard/[showcase_name]/[showcase_id]/videos/[video_id]/page.tsx
@@ -1,55 +1,55 @@
-"use client";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import styles from "./page.module.scss";
-import axios from "axios";
+'use client';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import styles from './page.module.scss';
+import axios from 'axios';
 
 const url = process.env.NEXT_PUBLIC_APP_URL;
 
 interface Video {
-	created_at: string;
-	description: string;
-	duration: number;
-	id: number;
-	playbackUrl: string;
-	showcase_id: string;
-	thumbnail_url: string;
-	title: string;
-	vimeo_video_id: number;
+  created_at: string;
+  description: string;
+  duration: number;
+  id: number;
+  playbackUrl: string;
+  showcase_id: string;
+  thumbnail_url: string;
+  title: string;
+  vimeo_video_id: number;
 }
 
 export default function SingleVideoPage() {
-	const { video_id } = useParams<{ video_id: string }>();
-	const [video, setVideo] = useState<Video | null>(null);
-	const [loading, setLoading] = useState(true);
+  const { video_id } = useParams<{ video_id: string }>();
+  const [video, setVideo] = useState<Video | null>(null);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		async function getVideo() {
-			try {
-				const response = await axios.get(`${url}/api/videos/${video_id}`);
-				setVideo(response.data);
-				setLoading(false);
-			} catch (error) {
-				console.error(`Unable to retrieve video details from vimeo: ${error}`);
-			}
-		}
+  useEffect(() => {
+    async function getVideo() {
+      try {
+        const response = await axios.get(`/api/videos/${video_id}`);
+        setVideo(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(`Unable to retrieve video details from vimeo: ${error}`);
+      }
+    }
 
-		getVideo();
-	}, [video_id]);
+    getVideo();
+  }, [video_id]);
 
-	if (loading) {
-		return <div>Loading video...</div>;
-	}
+  if (loading) {
+    return <div>Loading video...</div>;
+  }
 
-	return (
-		<div className={styles["video-container"]}>
-			<video controls poster={video?.thumbnail_url} className={styles.video}>
-				<source src={`${url}${video?.playbackUrl}`} />
-				Watch the
-				<a
-					href={`${url}${video?.playbackUrl}`}
-				>{`${video?.title} workout video`}</a>
-			</video>
-		</div>
-	);
+  return (
+    <div className={styles['video-container']}>
+      <video controls poster={video?.thumbnail_url} className={styles.video}>
+        <source src={`${url}${video?.playbackUrl}`} />
+        Watch the
+        <a
+          href={`${url}${video?.playbackUrl}`}
+        >{`${video?.title} workout video`}</a>
+      </video>
+    </div>
+  );
 }
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,132 +1,132 @@
-"use client";
+'use client';
 
-import { Suspense, useEffect, useState } from "react";
-import axios from "axios";
-import DashboardPageContent from "@/components/dashboard/DashboardPageContent/DashboardPageContent";
-import styles from "./page.module.scss";
-import AddToModal from "@/components/dashboard/AddToModal/AddToModal";
+import { Suspense, useEffect, useState } from 'react';
+import axios from 'axios';
+import DashboardPageContent from '@/components/dashboard/DashboardPageContent/DashboardPageContent';
+import styles from './page.module.scss';
+import AddToModal from '@/components/dashboard/AddToModal/AddToModal';
 
 const url = process.env.NEXT_PUBLIC_APP_URL;
 
 export interface Showcase {
-	id: number;
-	vimeo_showcase_id: string;
-	name: "string";
-	description: string;
-	thumbnail_url: string;
-	vimeo_link: string;
-	created_at: string;
+  id: number;
+  vimeo_showcase_id: string;
+  name: 'string';
+  description: string;
+  thumbnail_url: string;
+  vimeo_link: string;
+  created_at: string;
 }
 
 export default function DashboardPage() {
-	const [showcases, setShowcases] = useState<Showcase[]>([]);
-	const [isSelected, setIsSelected] = useState({
-		element: true,
-		program: false,
-	});
-	const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showcases, setShowcases] = useState<Showcase[]>([]);
+  const [isSelected, setIsSelected] = useState({
+    element: true,
+    program: false,
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	function handleSelect(selected: boolean) {
-		if (!selected) {
-			setIsSelected({
-				element: !isSelected.element,
-				program: !isSelected.program,
-			});
-		}
-	}
+  function handleSelect(selected: boolean) {
+    if (!selected) {
+      setIsSelected({
+        element: !isSelected.element,
+        program: !isSelected.program,
+      });
+    }
+  }
 
-	useEffect(() => {
-		async function getShowcases() {
-			try {
-				const response = await axios.get(`${url}/api/showcases`);
-				setShowcases(
-					response.data.showcases.filter(
-						(video: Showcase) =>
-							!video.name.toLowerCase().includes("body burning")
-					)
-				);
-			} catch (error) {
-				console.error(
-					`Unable to retrieve showcase videos from Vimeo: ${error}`
-				);
-			}
-		}
+  useEffect(() => {
+    async function getShowcases() {
+      try {
+        const response = await axios.get(`/api/showcases`);
+        setShowcases(
+          response.data.showcases.filter(
+            (video: Showcase) =>
+              !video.name.toLowerCase().includes('body burning')
+          )
+        );
+      } catch (error) {
+        console.error(
+          `Unable to retrieve showcase videos from Vimeo: ${error}`
+        );
+      }
+    }
 
-		getShowcases();
-	}, []);
+    getShowcases();
+  }, []);
 
-	//filter showcase videos by element or prescription programs
-	const filteredShowcases = showcases.filter((video: Showcase) =>
-		isSelected.program
-			? video.name.toLowerCase().includes("prescription")
-			: !video.name.toLowerCase().includes("prescription")
-	);
+  //filter showcase videos by element or prescription programs
+  const filteredShowcases = showcases.filter((video: Showcase) =>
+    isSelected.program
+      ? video.name.toLowerCase().includes('prescription')
+      : !video.name.toLowerCase().includes('prescription')
+  );
 
-	filteredShowcases.sort((a, b) => (a.name > b.name ? 1 : -1));
+  filteredShowcases.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-	return (
-		<Suspense fallback={<div>Loading dashboard...</div>}>
-			<div className={styles["tab-container"]}>
-				<ul className={styles.tabContent} role="tablist" tabIndex={0}>
-					<li
-						role="tab"
-						tabIndex={0}
-						id="element-tab"
-						aria-selected={isSelected.element}
-						aria-controls="element-panel"
-						className={`${styles.tab} ${
-							isSelected.element ? styles["tab--selected"] : ""
-						}`}
-						onClick={() => {
-							handleSelect(isSelected.element);
-						}}
-					>
-						elements
-					</li>
-					<li
-						role="tab"
-						tabIndex={0}
-						id="program-tab"
-						aria-selected={isSelected.program}
-						aria-controls="program-panel"
-						className={`${styles.tab} ${
-							isSelected.program ? styles["tab--selected"] : ""
-						}`}
-						onClick={() => {
-							handleSelect(isSelected.program);
-						}}
-					>
-						prescription programs
-					</li>
-				</ul>
-			</div>
-			<div
-				id="element-panel"
-				role="tabpanel"
-				aria-labelledby="element-tab"
-				hidden={!isSelected.element}
-			>
-				<DashboardPageContent
-					showcases={filteredShowcases}
-					type="element"
-					isModalOpen={isModalOpen}
-					setIsModalOpen={setIsModalOpen}
-				/>
-			</div>
-			<div
-				id="program-panel"
-				role="tabpanel"
-				aria-labelledby="program-tab"
-				hidden={!isSelected.program}
-			>
-				<DashboardPageContent
-					showcases={filteredShowcases}
-					type="program"
-					isModalOpen={isModalOpen}
-					setIsModalOpen={setIsModalOpen}
-				/>
-			</div>
-			<AddToModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-		</Suspense>
-	);
+  return (
+    <Suspense fallback={<div>Loading dashboard...</div>}>
+      <div className={styles['tab-container']}>
+        <ul className={styles.tabContent} role="tablist" tabIndex={0}>
+          <li
+            role="tab"
+            tabIndex={0}
+            id="element-tab"
+            aria-selected={isSelected.element}
+            aria-controls="element-panel"
+            className={`${styles.tab} ${
+              isSelected.element ? styles['tab--selected'] : ''
+            }`}
+            onClick={() => {
+              handleSelect(isSelected.element);
+            }}
+          >
+            elements
+          </li>
+          <li
+            role="tab"
+            tabIndex={0}
+            id="program-tab"
+            aria-selected={isSelected.program}
+            aria-controls="program-panel"
+            className={`${styles.tab} ${
+              isSelected.program ? styles['tab--selected'] : ''
+            }`}
+            onClick={() => {
+              handleSelect(isSelected.program);
+            }}
+          >
+            prescription programs
+          </li>
+        </ul>
+      </div>
+      <div
+        id="element-panel"
+        role="tabpanel"
+        aria-labelledby="element-tab"
+        hidden={!isSelected.element}
+      >
+        <DashboardPageContent
+          showcases={filteredShowcases}
+          type="element"
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      </div>
+      <div
+        id="program-panel"
+        role="tabpanel"
+        aria-labelledby="program-tab"
+        hidden={!isSelected.program}
+      >
+        <DashboardPageContent
+          showcases={filteredShowcases}
+          type="program"
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      </div>
+      <AddToModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+    </Suspense>
+  );
 }
