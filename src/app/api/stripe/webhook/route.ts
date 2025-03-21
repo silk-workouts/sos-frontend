ab1c6a16d7a46b730520d4028ecd3b2bd35884be
Matt Meyer <matt@mac.mynetworksettings.com>
2025-03-21 17:20:53 -0400

    udpate stripe apiVersion used in stripe/webhook based on finding in Stripe dashboard

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -5,8 +5,11 @@ import pool from "@/lib/db";
 
 console.log(`Node.js version: ${process.version}`);
 
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2023-10-16",
+// });
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2023-10-16" as any, // Loosen type enforcement
 });
 
 export const config = {
