d186c6ec498fd9d0f9136dd93ce390998e5d7a12
Matt Meyer <matt@Matts-MacBook-Air.local>
2025-03-21 14:17:13 -0400

    update #4 vercel.json file due to build error detected when pushed, add node version check to api/stripe/webhook route incase needed

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import pool from "@/lib/db";
 
+console.log(`Node.js version: ${process.version}`);
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-01-27.acacia",
 });
