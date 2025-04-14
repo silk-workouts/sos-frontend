8641d999b9fd3c997378f54983733c959f24a1f2
Ife Onuorah <ifeonuorah@gmail.com>
2025-04-13 21:51:48 -0300

    Update the element filter in dashboard to include a clear filter button

--- src/components/pages/dashboard/ElementFilter/ElementFilter.tsx
+++ src/components/pages/dashboard/ElementFilter/ElementFilter.tsx
@@ -1,42 +1,56 @@
+import Button from "@/components/ui/Button/Button";
 import { Filter } from "../DashboardPageContent/DashboardPageContent";
 import styles from "./ElementFilter.module.scss";
 
 interface FilterProps {
-	elementFilter: Set<string>;
-	handleFilter: (s: string) => void;
-	options: Filter[];
+  elementFilter: Set<string>;
+  setFilter: (arg: Set<string>) => void;
+  handleFilter: (s: string) => void;
+  options: Filter[];
 }
 
 export default function ElementFilter({
-	elementFilter,
-	handleFilter,
-	options,
+  elementFilter,
+  setFilter,
+  handleFilter,
+  options,
 }: FilterProps) {
-	return (
-		<ul
-			className={styles.list}
-			role="tablist"
-			tabIndex={0}
-			aria-multiselectable="true"
-			aria-label="Filter workout by element type"
-		>
-			{options.map((option) => {
-				const isSelected = elementFilter.has(option.name);
-				return (
-					<li
-						key={option.id}
-						className={`${styles.item} ${
-							isSelected ? styles["item--selected"] : ""
-						}`}
-						role="tab"
-						tabIndex={isSelected ? 0 : -1}
-						aria-selected={isSelected}
-						onClick={() => handleFilter(option.name)}
-					>
-						{option.name}
-					</li>
-				);
-			})}
-		</ul>
-	);
+  return (
+    <ul
+      className={styles.list}
+      role="tablist"
+      tabIndex={0}
+      aria-multiselectable="true"
+      aria-label="Filter workout by element type"
+    >
+      {options.map((option) => {
+        const isSelected = elementFilter.has(option.name);
+        return (
+          <li
+            key={option.id}
+            className={`${styles.item} ${
+              isSelected ? styles["item--selected"] : ""
+            }`}
+            role="tab"
+            tabIndex={isSelected ? 0 : -1}
+            aria-selected={isSelected}
+            onClick={() => handleFilter(option.name)}
+          >
+            {option.name}
+          </li>
+        );
+      })}
+      {options.length > 0 && (
+        <li>
+          <Button
+            variant="text"
+            onClick={() => setFilter(new Set())}
+            className={styles.button}
+          >
+            Clear filter
+          </Button>
+        </li>
+      )}
+    </ul>
+  );
 }
